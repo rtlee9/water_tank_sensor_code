9f9928228bbd9efab964c56d01799944ac46278a
Ryan Lee <ryantlee9@gmail.com>
2022-05-10 23:53:41 -0700

    Switch to aruino nano
    Update wifi and mqtt libraries
    Switch to Adafruit IO

--- water_tank_sensor_code.ino
+++ water_tank_sensor_code.ino
@@ -1,13 +1,15 @@
 #include "secrets.h"
-#include <WiFiClientSecure.h>
 #include <MQTTClient.h>
 #include <ArduinoJson.h>
-#include "WiFi.h"
-#include <U8x8lib.h>
+#include <WiFiNINA.h>
+#include <SPI.h>
+#include <PubSubClient.h>
+
 
 // The MQTT topics that this device should publish/subscribe
-#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+#define PUBLISH_TOPIC   "rtlee/feeds/test"
+#define TEST_TOPIC   "rtlee/feeds/test"
+#define SUBSCRIBE_TOPIC "rtlee/feeds/sub"
 #define ANALOG_PIN 33
 #define VREF 3300 // ADC's reference voltage on your Arduino,typical value:5000mV
 #define CURRENT_INIT 4.00 // Current @ 0mm (uint: mA)
@@ -21,82 +23,21 @@
 #define NUM_READINGS 50
 
 uint32_t pub_frequency = 60000; // ms frequency of publishing sensor readings
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
+WiFiClient wifiClient;
+long lastReconnectAttempt = 0;
+PubSubClient client("io.adafruit.com", 1883, wifiClient);
 
-// the OLED used
-U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
-
-WiFiClientSecure net = WiFiClientSecure();
-MQTTClient client = MQTTClient(256);
-
-void connectWifi()
-{
-  delay(500);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  Serial.println("Connecting to Wi-Fi");
-  u8x8.drawString(0, 0, "Connecting wifi");
-
-  uint32_t notConnectedCounter = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-    Serial.print(".");
-    notConnectedCounter++;
-    if(notConnectedCounter > 100) { // Reset board if not connected after 10s
-      Serial.println("Resetting due to Wifi not connecting...");
-      ESP.restart();
-    }
+boolean reconnect() {
+  Serial.println("Attempting to connect to MQTT client");
+  if (client.connect("arduinoClient", ADAFRUIT_USERNAME, ADAFRUIT_PASSWORD)) {
+    Serial.println("Connected to MQTT client");
+    // Once connected, publish an announcement...
+    // client.publish(TEST_TOPIC,"hello world");
+    // ... and resubscribe
+    // client.subscribe(SUBSCRIBE_TOPIC);
   }
-
-  // Configure WiFiClientSecure to use the AWS IoT device credentials
-  net.setCACert(AWS_CERT_CA);
-  net.setCertificate(AWS_CERT_CRT);
-  net.setPrivateKey(AWS_CERT_PRIVATE);
-}
-
-void connectAWS()
-{
-  connectWifi();
-  connectMQTT();
-}
-
-void connectMQTT()
-{
-  // Connect to the MQTT broker on the AWS endpoint we defined earlier
-  client.begin(AWS_IOT_ENDPOINT, 8883, net);
-  client.setKeepAlive(120);
-
-  // Create a message handler
-  client.onMessage(messageHandler);
-
-  Serial.println("Connecting to AWS IOT");
-  u8x8.clearLine(0);
-  u8x8.drawString(0, 0, "Connecting AWS");
-
-  int i = 0;
-  while (!client.connect(THINGNAME)) {
-    Serial.print(".");
-    u8x8.drawString(i, 1, ".");
-    delay(100);
-    i = i + 1;
-    if (i = 3) {
-      i = 0;
-      u8x8.clearLine(1);
-    }
-  }
-
-  if (!client.connected()) {
-    Serial.println("AWS IoT Timeout!");
-    return;
-  }
-
-  // Subscribe to a topic
-  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
-
-  Serial.println("AWS IoT Connected!");
-  u8x8.clearLine(0);
-  u8x8.clearLine(1);
-  u8x8.drawString(0, 0, "AWS connected");
+  return client.connected();
 }
 
 
@@ -134,34 +75,64 @@ void publishMessage()
 
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
-  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+  client.publish(PUBLISH_TOPIC, jsonBuffer);
   
   Serial.println(jsonBuffer);  // print to serial
-  u8x8.clearLine(0);
-  u8x8.drawString(0, 0, strcat(itoa(depth / MM_PER_INCH, buf, 10), "\" depth"));
-}
-
-void messageHandler(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-
-  // deserialize json
-  StaticJsonDocument<200> doc;
-  deserializeJson(doc, payload);
-
-  if (doc.containsKey("pub_frequency")) {
-    pub_frequency = int(doc["pub_frequency"]);
-  }
 }
 
 void setup() {
+  // Initialize serial and wait for port to open:
   Serial.begin(9600);
-  u8x8.begin();
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  connectAWS();
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(WIFI_SSID);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    // wait 5 seconds for connection:
+    delay(5000);
+  }
+
+
+  Serial.println("You're connected to the network");
+  lastReconnectAttempt = 0;
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // handle message arrived
 }
 
 void loop() {
-  client.loop();
-  publishMessage();
-  delay(pub_frequency);
+    if (!client.connected()) {
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+    // Client connected
+    client.loop();
+    publishMessage();
+    delay(5000);
+  }
 }
