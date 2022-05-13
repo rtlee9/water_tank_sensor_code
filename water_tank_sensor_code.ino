fe73f4a6462f0bfc11d9fd55942349d9561aef74
Ryan Lee <ryantlee9@gmail.com>
2022-05-12 21:32:50 -0700

    Remove serial wait bug
    Was hanging when not connected to computer

--- water_tank_sensor_code.ino
+++ water_tank_sensor_code.ino
@@ -83,9 +83,6 @@ void publishMessage()
 void setup() {
   // Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
   
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
